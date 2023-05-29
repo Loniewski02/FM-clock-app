b7079ed931f7a9cc7bd232ae12c4f1fa62702243
Milosz <miloszloniewski02@gmail.com>
2023-05-29 19:32:15 +0200

    added random quotes

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,41 @@
 let showBtn;
+let appSection;
+let refreshQuoteBtn;
+
+const URL_QUOTE = `https://api.quotable.io/random`;
 
 const prepareDOMElements = () => {
 	showBtn = document.querySelector('.app__clock-btn');
+	appSection = document.querySelector('.app');
+	refreshQuoteBtn = document.querySelector('.app__quote-btn');
 };
 
 const prepareDOMEvents = () => {
 	showBtn.addEventListener('click', handleInfo);
+	refreshQuoteBtn.addEventListener('click', handleQuote);
 };
 
 const handleInfo = () => {
-	showBtn.classList.toggle('btn-active');
-	if (showBtn.classList.contains('btn-active')) {
+	appSection.classList.toggle('app--active');
+	const btnText = document.querySelector('.app__clock-btn-text');
+	if (appSection.classList.contains('app--active')) {
 		showDetails();
+		btnText.textContent = 'less';
+		gsap.to('.app__clock-btn-circle', {
+			duration: 0.3,
+			rotate: '180deg',
+		});
 	} else {
 		hideDetails();
+		btnText.textContent = 'more';
+		gsap.to('.app__clock-btn-circle', {
+			duration: 0.3,
+			rotate: '0',
+		});
 	}
 };
 
 const showDetails = () => {
-	console.log('ok');
 	gsap.to('.app-bottom', {
 		duration: 0.5,
 		y: '-100%',
@@ -38,9 +55,34 @@ const hideDetails = () => {
 	});
 };
 
+async function handleQuote() {
+	const response = await axios.get(URL_QUOTE);
+	try {
+		const quoteText = document.querySelector('.app__quote-text');
+		const quoteAuthor = document.querySelector('.app__quote-author');
+		quoteText.textContent = response.data.content;
+		quoteAuthor.textContent = response.data.author;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function getTime() {
+	const response = await axios.get(URL_QUOTE);
+	try {
+		const quoteText = document.querySelector('.app__quote-text');
+		const quoteAuthor = document.querySelector('.app__quote-author');
+		quoteText.textContent = response.data.content;
+		quoteAuthor.textContent = response.data.author;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvents();
+	handleQuote();
 };
 
 document.addEventListener('DOMContentLoaded', main);
